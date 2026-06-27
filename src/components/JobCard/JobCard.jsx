a6b1c755a74791c294e1f5cce6730f38a06d67e5
Arihantdesai165 <arihantdesai483@gmail.com>
2026-06-27 23:06:19 +0530

    feat: display relative posted time on JobCard using timeAgo helper

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { HiOutlineLocationMarker, HiOutlineCurrencyDollar, HiOutlineBriefcase, HiOutlineChip } from 'react-icons/hi';
-import { formatSalary } from '../../utils/helpers';
+import { formatSalary, timeAgo } from '../../utils/helpers';
 import './JobCard.css';
 
 const JobCard = ({ job }) => {
-  const { title, location, salary, skillsRequired, experienceRequired, description } = job;
+  const { title, location, salary, skillsRequired, experienceRequired, description, createdAt } = job;
 
   return (
     <div className="job-card glass-card">
@@ -12,6 +12,11 @@ const JobCard = ({ job }) => {
         <h3 className="job-card__title">{title}</h3>
         <span className="badge badge-primary">{experienceRequired}Y Exp</span>
       </div>
+      {createdAt && (
+        <div className="job-card__time">
+          <span>Posted {timeAgo(createdAt)}</span>
+        </div>
+      )}
       
       <p className="job-card__description">{description}</p>
       
