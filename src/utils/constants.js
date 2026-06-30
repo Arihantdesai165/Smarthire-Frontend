53c92793fe9ae3479c5b4b6547592f69b690c6f8
Arihantdesai165 <arihantdesai483@gmail.com>
2026-06-30 21:59:07 +0530

    chore: refine constants file - part 4

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,3 +5,4 @@ export const API_TIMEOUT = 5000;
 // chore: code refinement step 1782836945802_1
 // chore: code refinement step 1782836946223_2
 // chore: code refinement step 1782836946516_3
+// chore: code refinement step 1782836946809_4
